eb519e22cc28ec9764a6d124a1daeb0514d3961f
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-09-26 18:52:50 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+  name='district42',
+  version='0.1',
+  url='https://github.com/nikitanovosibirsk/district42',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=['district42']
+)
