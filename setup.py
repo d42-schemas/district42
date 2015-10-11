fb12ba628465d159a9ff671e9264363be92d9312
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-11 21:59:43 +0600

    update version

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
   name='district42',
-  version='0.2.0',
+  packages=['district42', 'district42/json_schema'],
+  version='0.5.2',
+  description='',
   url='https://github.com/nikitanovosibirsk/district42',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
-  license='MIT',
-  packages=['district42']
+  license='MIT'
 )
