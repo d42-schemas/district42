e9a50bafd0a7d25278f0af45c2ee3b90498f8e5d
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-19 16:57:24 +0600

    bump version: 0.5.3 -> 0.5.4

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup, find_packages
 setup(
   name='district42',
   description='Data description language for defining data models',
-  version='0.5.3',
+  version='0.5.4',
   url='https://github.com/nikitanovosibirsk/district42',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
-  packages=find_packages()
+  packages=find_packages(),
+  install_requires=[
+    'delorean==0.5.0'
+  ]
 )
