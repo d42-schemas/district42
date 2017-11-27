d39db770d53fd36cf62b31b55944aca719f4df71
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2017-11-27 18:38:35 +0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
   name='district42',
   description='Data description language for defining data models',
-  version='0.5.5',
+  version='0.6.1',
   url='https://github.com/nikitanovosibirsk/district42',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
