098edd3e64d850f085df823166abd6e678e72642
Nikita Tsvetkov <tsv1@fastmail.com>
2023-08-16 18:31:01 +0400

    add rollout

--- district42/utils/__init__.py
+++ district42/utils/__init__.py
@@ -1,17 +1,4 @@
-from typing import TYPE_CHECKING, Any, TypeVar, Union
+from ._is_ellipsis import EllipsisType, TypeOrEllipsis, is_ellipsis
+from ._rollout import rollout
 
-__all__ = ("is_ellipsis", "EllipsisType", "TypeOrEllipsis",)
-
-if TYPE_CHECKING:
-    import builtins
-    EllipsisType = builtins.ellipsis
-else:
-    EllipsisType = Any
-
-
-def is_ellipsis(value: Any) -> bool:
-    return isinstance(value, type(...))
-
-
-_T = TypeVar("_T")
-TypeOrEllipsis = Union[_T, EllipsisType]
+__all__ = ("is_ellipsis", "EllipsisType", "TypeOrEllipsis", "rollout",)
