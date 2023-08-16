c31f227504c58d0b37f84961aa9c322f9a23d041
Nikitа Tsvetkоv <tsv1@fastmail.com>
2023-08-16 18:40:23 +0400

    add rollout (#13)

--- district42/types/_dict_schema.py
+++ district42/types/_dict_schema.py
@@ -7,29 +7,10 @@ from .._schema_visitor import SchemaVisitor
 from .._schema_visitor import SchemaVisitorReturnType as ReturnType
 from ..errors import DeclarationError, make_already_declared_error, make_invalid_type_error
 from ..utils import TypeOrEllipsis, is_ellipsis
+from ._optional import optional
 from ._schema import GenericSchema, Schema
 
 __all__ = ("DictSchema", "DictProps", "make_required", "optional",)
-
-
-class optional:
-    def __init__(self, key: Any) -> None:
-        if is_ellipsis(key):
-            raise TypeError(key)
-        self._key = key
-
-    @property
-    def key(self) -> Any:
-        return self._key
-
-    def __repr__(self) -> str:
-        return f"optional({self._key!r})"
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, self.__class__) and (self._key == other.key)
-
-    def __hash__(self) -> int:
-        return hash((self._key,))
 
 
 class DictProps(Props):
