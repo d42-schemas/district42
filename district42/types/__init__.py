e0c5c8f206f97701fee769aeff8cd6763209386c
YevgeniyBelozerov <yevgeniy.belozerov@gmail.com>
2024-07-16 13:53:03 +0700

    add date type

--- district42/types/__init__.py
+++ district42/types/__init__.py
@@ -2,6 +2,7 @@ from ._any_schema import AnyProps, AnySchema
 from ._bool_schema import BoolProps, BoolSchema
 from ._bytes_schema import BytesProps, BytesSchema
 from ._const_schema import ConstProps, ConstSchema
+from ._date_schema import DateProps, DateSchema
 from ._datetime_schema import DateTimeProps, DateTimeSchema
 from ._dict_schema import DictProps, DictSchema, make_required, optional
 from ._float_schema import FloatProps, FloatSchema
@@ -21,7 +22,7 @@ from ._uuid4_schema import UUID4Props, UUID4Schema
 __all__ = ("AnyProps", "AnySchema", "BoolProps", "BoolSchema", "BytesProps", "BytesSchema",
            "ConstProps", "ConstSchema", "DictProps", "DictSchema", "FloatProps", "FloatSchema",
            "IntProps", "IntSchema", "ListProps", "ListSchema", "NoneProps", "NoneSchema",
-           "StrProps", "StrSchema", "UUID4Props", "UUID4Schema", "DateTimeProps", "DateTimeSchema",
-           "TypeAliasSchema", "TypeAliasProps",
+           "StrProps", "StrSchema", "UUID4Props", "UUID4Schema", "DateProps", "DateSchema",
+           "DateTimeProps", "DateTimeSchema", "TypeAliasSchema", "TypeAliasProps",
            "GenericTypeAliasSchema", "TypeAliasPropsType", "GenericSchema", "Schema",
            "optional", "make_required", )
